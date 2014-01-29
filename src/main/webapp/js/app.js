5e227888e68fd0ab9d60580437631baf45c54bdc
Eric Bloch <eric.bloch@gmail.com>
2014-01-29 15:16:33 -0500

    unit tests and working web ui and spec

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,117 @@
+var app = angular.module('app', []);
+
+var page = 1;
+
+app.controller('queryCtrl', function($scope, $http) {
+	$scope.results = [];
+	$scope.selected = {};
+	$scope.selectedCount = 0;
+	$scope.message = undefined;
+
+	$scope.search = function() {
+		page = 1;
+		$scope.results = [];
+		$scope.isSearching = true;
+		$http.get('search?q=' + encodeURIComponent($('#query').val()) + '&p=' + page).success(
+			function(data) {
+				$scope.isSearching = false;
+				$scope.results = data.results;
+				$scope.total = data.totalResults;
+				$scope.facets = data.facets;
+				$scope.selected = {};
+				$scope.selectedCount = 0;
+			}).error(function() {
+				$scope.isSearching = false;
+			});
+		page = page + 1;
+	};
+
+	$scope.scroll = function() {
+		if ($scope.isScrolling)
+			return;
+		
+		$scope.isScrolling = true;
+		$http.get('search?q=' + encodeURIComponent($('#query').val()) + '&p=' + page).success(
+			function(data) {
+				$scope.isScrolling = false;
+				for ( var i = 0; i < data.results.length; i++) {
+					$scope.results.push(data.results[i]);
+				}
+				if (data.results.length > 0) {
+					page = page + 1;
+				}
+			}).error(function() {
+				$scope.isScrolling = false;
+			});
+		
+	};
+
+	$scope.updateSelection = function($event, uri) {
+		var checkbox = $event.target;
+
+		if (checkbox.checked) {
+			$scope.selected[uri] = 1;
+			$scope.selectedCount += 1;
+		} else {
+			delete $scope.selected[uri];
+			$scope.selectedCount -= 1;
+		}
+
+	};
+
+	$scope.tag = function() {
+		if ($scope.selectedCount <= 0)
+			return;
+
+		$scope.isTagging = true;
+		$http.post('tag?t=' + encodeURIComponent($('#tag-text').val()),
+			Object.keys($scope.selected)).success(function(data) {
+			$scope.isTagging = false;
+			$scope.tags = data.count;
+			$scope.search();
+		}).error(function() {
+			$scope.isTagging = false;
+			alert("Tagging failed!");
+		});
+		
+	};
+
+	$scope.showMessage = function(uri) {
+		$http.get('message' + uri).success(function(data) {
+			$scope.message = data;
+		}).error(function() {
+			alert("Can't fetch " + uri);
+		});
+	};
+
+	$scope.unimplemented = function() {
+		alert("Unimplemented!!");
+	};
+
+});
+
+app.directive('onEnter', function() {
+	return function(scope, element, attrs) {
+		element.bind("keydown keypress", function(event) {
+			if (event.which === 13) {
+				scope.$apply(function() {
+					scope.$eval(attrs.onEnter);
+				});
+				event.preventDefault();
+			}
+		});
+	};
+});
+
+app.directive('whenScrolled', function() {
+	return function(scope, elm, attr) {
+		var raw = elm[0];
+
+		elm.bind('scroll', function() {
+			console.log('scroll');
+			if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+				scope.$apply(attr.whenScrolled);
+			}
+		});
+	};
+});
---
+++ src/test/java/com/marklogic/dx/utjava/ApiTest.java
@@ -0,0 +1,167 @@
+package com.marklogic.dx.utjava;
+
+import junit.framework.TestCase;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.response.ValidatableResponse;
+
+
+import static com.jayway.restassured.RestAssured.get;
+import static com.jayway.restassured.RestAssured.post;
+import static com.jayway.restassured.RestAssured.given;
+
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
+
+
+
+
+/**
+ * Some very basic API tests that are dependent on the default data set of 1862 email messages.
+ * 
+ * Tests are designed to be idempotent and not to break given any early tagging (there is some
+ * possibility that making tags with words that are used as search terms in the test could cause issues).
+ * 
+ * @author ebloch
+ *
+ */
+public class ApiTest extends TestCase {
+
+		@Override
+		public void setUp() {
+			
+			// TODO: bring in system properties for connection details
+			
+			RestAssured.basePath = "/exercise-1";
+		}
+		
+		@Override
+		public void tearDown() {
+			
+		}
+		
+		public void testMessage() {
+			
+			get("/message/foo.xml").then().assertThat().statusCode(404);
+			
+			ValidatableResponse response = get("/message/mail/a3x5yi4l7iypy7ka.xml").then();
+			
+			response.assertThat().statusCode(200);
+			response.assertThat().contentType(ContentType.JSON);
+			
+			response.body("subject", equalTo("FYI: MarkMail now indexes users@sling.apache.org"));
+			
+			response.body("from", equalTo("vidar@idium.no"));
+
+			response.body("date", equalTo("2010-02-16"));
+			
+			response.body("list", equalTo("org.apache.sling.users"));
+			
+		}
+		
+		/**
+		 * Pretty basic tests
+		 */
+		public void testSearch() {
+			
+			ValidatableResponse response = get("/search?q=&p=").then();
+			
+			response.assertThat().statusCode(200);
+			response.assertThat().contentType(ContentType.JSON);
+			
+			response.body("totalResults", equalTo(1862));
+			
+			response.body("results.findall.size", equalTo(10));
+			
+			response.body("p", equalTo(1));
+			
+			response = get("/search?q=hello&p=").then();
+			
+			/* Should really devise a test that hits on each of the specific hit areas in the doc */
+			response.body("totalResults", equalTo(9));
+			
+			response.body("results[0].list", equalTo("org.openlaszlo.laszlo-user"));
+			response.body("results[0].date", equalTo("2005-01-20"));
+			response.body("results[0].subj", equalTo("[Laszlo-user] Dynamic Production of .lzx documents"));
+			response.body("results[0].pers", equalTo("Eric Bloch"));
+			response.body("results[0].from", equalTo("bloch@laszlosystems.com"));
+			response.body("results[0].uri", equalTo("/mail/zrjwjnpkev2zbzxt.xml"));
+			response.body("results[0].snippet", equalTo(			
+					"\n      where the compiler would look for a file called hello.jsp and GET it, \nassume the response is text/xml containing LZX code and...\n      ...examples/hello.jsp for you to look at but, again, this...\n    "
+			));	
+		}
+		
+		public void testTag() {
+			
+			Response res = post("/tag?t=");
+			ValidatableResponse response = res.then();
+			
+			response.assertThat().statusCode(200);
+			response.assertThat().contentType(ContentType.JSON);
+			
+			assertEquals(res.asString(), "\"0\"");
+			
+			String uris = toJSON(new String[] { "/foo/bar.xml" } );
+			
+			res = given().body(uris).post("/tag?t=hello");
+			response = res.then();
+			
+			response.assertThat().statusCode(400);
+			
+			/* Tag 2 docs */
+			uris = toJSON( new String[] {
+					  "/mail/zrjwjnpkev2zbzxt.xml", 
+					  "/mail/pfo6x6w7kestl5yg.xml"  
+					}) ;
+			
+			res = given().body(uris).post("/tag?t=hello");
+			response = res.then();
+			
+			response.assertThat().statusCode(200);
+			assertEquals(res.asString(), "\"2\"");
+			
+			response = get("/message/mail/zrjwjnpkev2zbzxt.xml").then();
+			response.body("tags", hasItem(equalTo("hello")));
+			
+			response = get("/message/mail/pfo6x6w7kestl5yg.xml").then();
+			response.body("tags", hasItem(equalTo("hello")));
+			
+			/* One of these docs will fail, so both should fail */
+			uris = toJSON( new String[] {
+					  "/mail/zrjwjnpkev2zbzxt.xml", 
+					  "/mail/bad.xml"  
+					}) ;
+			
+			res = given().body(uris).post("/tag?t=bad");
+			response = res.then();
+			
+			response.assertThat().statusCode(400);
+			
+			/* Test that the tag did not get set */
+			response = get("/message/mail/zrjwjnpkev2zbzxt.xml").then();
+			response.body("tags", not(hasItem(equalTo("bad"))));
+			response.body("tags", hasItem(equalTo("hello")));
+
+
+		}
+		
+		static public String toJSON(Object x) {
+			ObjectMapper mapper = new ObjectMapper(); 
+
+			String str;
+			try {
+				str = mapper.writeValueAsString(x);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+				str = "\"Exception!\"";
+			}
+			return str;
+		}
+	
+}
